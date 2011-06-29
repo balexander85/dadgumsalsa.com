dd47c2e4fbb19e229a0f16aa03bff18e924c850c
Brian Alexander <dadgumsalsa@gmail.com>
2011-06-28 22:08:49 -0500

    Did some serious overhauls on the links and rollover images

--- index.php
+++ index.php
@@ -6,44 +6,26 @@
 <div id="image_map">
 
 <a href="dadgumAbout.php" id="link1">1
-<span class="freshRollover">
+<span class="leftRollover">
 <img alt="About Dad Gum" src="menu_items/about_Menu.png" />
 </span>
 </a>
 
 <a href="dadgumBlog.php" id="link2">2
-<span class="freshRollover">
+<span class="leftRollover">
 <img alt="Dad Gum Photo Blog" src="menu_items/photo_Gallery.png" />
 </span>
 </a>
 
 <a href="dadgumOrder.php" id="link3">3
-<span class="freshRollover">
+<span class="leftRollover">
 <img alt="Order Dad Gum" src="menu_items/order_Menu.png" />
 </span>
 </a>
 
 <a href="dadgumGallery.php" id="link4">4
-<span class="freshRollover">
+<span class="leftRollover">
 <img alt="Fresh Rollover" src="menu_items/photo_Gallery.png" />
-</span>
-</a>
-
-<a href="#nogo" id="link5">5
-<span class="jap3Rollover">
-<img id="popupImg3" alt="Jap4" src="images/Jap3.png" />
-</span>
-</a>
-
-<a href="dadgumBlog.php" id="link6">6
-<span class="jap4Rollover">
-<img id="popupImg4" alt="Jap3" src="images/Jap4.png" />
-</span>
-</a>
-
-<a href="dadgumAbout.php" id="link7">7
-<span class="popupBlog">
-<img id="popupBlog" alt="blog" src="images/tomato_Resize.png" />
 </span>
 </a>
 
