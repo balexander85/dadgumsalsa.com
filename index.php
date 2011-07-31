a5ecedb63a0725cdf3e1fa488d9045feb5574633
Brian Alexander <dadgumsalsa@gmail.com>
2011-07-31 16:18:46 -0500

    I removed old rollover links. Added the other menulinks to the page.

--- index.php
+++ index.php
@@ -3,43 +3,26 @@
 
 <div id="menuBox">
 
-<!--<div id="image_map">-->
-
 <div id="about_MenuLink">
 <a class="linkopacity" href="dadgumAbout.php"> 
-<img alt="About Dad Gum" src="menu_items/about_Menu.png" />
+<img alt="About Dad Gum" src="menu_items/about_Menu.png"/>
 </a>
 </div>
-<!--
-<a href="dadgumAbout.php" id="link1">1
-<span class="leftRollover">
-<img alt="About Dad Gum" src="menu_items/about_Menu.png" />
-</span>
-</a>
 
-<a href="dadgumBlog.php" id="link2">2
-<span class="leftRollover">
-<img alt="Dad Gum Photo Blog" src="menu_items/photo_Gallery.png" />
-</span>
+<div id="gallery_MenuLink">
+<a class="linkopacity" href="dadgumGallery.php">
+<img alt="Dad Gum Photo Gallery" src="menu_items/photo_Gallery.png"/>
 </a>
+</div>
 
-<a href="dadgumOrder.php" id="link3">3
-<span class="leftRollover">
-<img alt="Order Dad Gum" src="menu_items/order_Menu.png" />
-</span>
+<div id="order_MenuLink">
+<a class="linkopacity" href="dadgumOrder.php">
+<img alt="Dad Gum Order" src="menu_items/order_Menu.png" />
 </a>
+</div>
 
-<a href="dadgumGallery.php" id="link4">4
-<span class="leftRollover">
-<img alt="Fresh Rollover" src="menu_items/photo_Gallery.png" />
-</span>
-</a>
-
-<a id="link5" class="photo-link">5
-</a>
--->
-
-</div><!--End of menuBox div-->
+</div>
+<!--End of menuBox div-->
 
 
 <?php include "dadGumFooter.php";?>
