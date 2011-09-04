886da4def3dce491d26f1e5460d2324083dcf060
Brian Alexander <dadgumsalsa@gmail.com>
2011-09-04 15:15:03 -0500

    Working on the design for the website

---
+++ newDadgumsite.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link type="image/png" rel="shortcut icon" href="resources/tomato_Favicon.png"/>
+<meta name="author" content="Brian Alexander" />
+<meta name="description" content="Dad Gum Salsa Coming Soon!" />
+<meta name="keywords" content="Dad Gum, Salsa, Dad Gum Salsa, Chips and Salsa" />  
+<title>New Dad Gum Website</title>
+<!--Cascading Style Sheets for this Dad'Gum Website--> 
+</head>
+<body>
+
+
+
+
+</body>
+</html>
