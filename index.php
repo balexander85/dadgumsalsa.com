d8986083aa7ccc9aba2dfadebd841560979edccd
Brian Alexander <Brian.Alexander@myedu.com>
2011-10-14 22:56:12 -0500

    made some adjustments, cleaned up some code

--- index.php
+++ index.php
@@ -3,19 +3,19 @@
 
 <div id="menuBox">
 
-<div id="about_MenuLink">
+<div class="menuLink" id="about_MenuLink">
 <a class="linkopacity" href="dadgumAbout.php"> 
 <img alt="About Dad Gum" src="menu_items/about_Menu.png"/>
 </a>
 </div>
 
-<div id="gallery_MenuLink">
+<div class="menuLink" id="gallery_MenuLink">
 <a class="linkopacity" href="dadgumGallery.php">
 <img alt="Dad Gum Photo Gallery" src="menu_items/photo_Gallery.png"/>
 </a>
 </div>
 
-<div id="order_MenuLink">
+<div class="menuLink" id="order_MenuLink">
 <a class="linkopacity" href="dadgumOrder.php">
 <img alt="Dad Gum Order" src="menu_items/order_Menu.png" />
 </a>
