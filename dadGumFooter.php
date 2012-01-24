dbd59b89be13cdced84497e5737f00f4f435d5f2
Brian Alexander <balexander04@gmail.com>
2012-01-23 22:38:17 -0600

    Reloading the repo

---
+++ dadGumFooter.php
@@ -0,0 +1,7 @@
+<div id="footerPage">
+
+<a href="mailto:info@dadgumsalsa.com"><div id="contactUs"></div></a>
+
+</div>
+</body> 
+</html>
---
+++ index.php
@@ -0,0 +1,55 @@
+<?php include "dadGumHeader.php";?>
+
+
+<div id="menuBox">
+
+<div id="image_map">
+<!--
+<a href="dadgumAbout.php" id="link1">1
+<span class="freshRollover">
+<img alt="About Dad Gum" src="menu_items/about_Menu.png" />
+</span>
+</a>
+
+<a href="dadgumBlog.php" id="link2">2
+<span class="freshRollover">
+<img alt="Dad Gum Photo Blog" src="menu_items/blog_Menu.png" />
+</span>
+</a>
+
+<a href="dadgumOrder.php" id="link3">3
+<span class="freshRollover">
+<img alt="Order Dad Gum" src="menu_items/order_Menu.png" />
+</span>
+</a>
+-->
+<a href="dadgumGallery.php" id="link4">
+<span class="freshRollover">
+<img alt="Fresh Rollover" src="menu_items/photo_Gallery.png" />
+</span>
+</a>
+<!--
+<a href="#nogo" id="link5">5
+<span class="jap3Rollover">
+<img id="popupImg3" alt="Jap4" src="images/Jap3.png" />
+</span>
+</a>
+
+<a href="dadgumBlog.php" id="link6">6
+<span class="jap4Rollover">
+<img id="popupImg4" alt="Jap3" src="images/Jap4.png" />
+</span>
+</a>
+
+<a href="dadgumAbout.php" id="link7">7
+<span class="popupBlog">
+<img id="popupBlog" alt="blog" src="images/tomato_Resize.png" />
+</span>
+</a>
+-->
+</div> 
+
+</div>
+
+
+<?php include "dadGumFooter.php";?>
