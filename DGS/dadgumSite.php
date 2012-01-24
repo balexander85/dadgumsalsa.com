84921ffe5ef66f3da34239c68a51dc50a9130c4b
Brian Alexander <balexander04@gmail.com>
2012-01-23 22:20:23 -0600

    Removing DGS directory, I need to maybe make that a different branch or a different repo altogether

--- DGS/dadgumSite.php
+++
@@ -1,9 +0,0 @@
-
-<?php include "DGSheader.php";?>
-
-<a class="" href="http://www.google.com">
-<img id="frontImage" src="images/DGSfrontImage.png" alt="Image Displayed" />
-</a>
-
-</body>
-</html>
