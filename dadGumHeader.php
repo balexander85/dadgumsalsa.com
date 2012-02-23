f39427408632b32a85e33e547c991d0c8d78f021
Brian Alexander <balexander04@gmail.com>
2012-02-22 21:29:57 -0600

    Made some changes to a little bit of everything, tried to style the order page, need to add more form elements

--- dadGumHeader.php
+++ dadGumHeader.php
@@ -34,7 +34,7 @@ if (preg_match($blog_pattern, $urlValue)) {
 } elseif (preg_match($about_pattern, $urlValue)) {
 	$page = "About";
 } elseif (preg_match($order_pattern, $urlValue)) {
-	$page = "Order";
+	$page = "Order Page";
 } elseif (preg_match($gallery_pattern, $urlValue)) {
 	$page = "Photo Gallery";
 } else {
@@ -86,7 +86,42 @@ if (preg_match($blog_pattern, $urlValue)) {
 <script type="text/javascript">
     // this identifies your website in the createToken call below
     Stripe.setPublishableKey('pk_QER18kLZvhxEoM0oNGISon8fM5onr');
-    ...
+
+    function stripeResponseHandler(status, response) {
+        if (response.error) {
+            // re-enable the submit button
+            $('.submit-button').removeAttr("disabled");
+            // show the errors on the form
+            $(".payment-errors").html(response.error.message);
+        } else {
+            var form$ = $("#payment-form");
+            // token contains id, last4, and card type
+            var token = response['id'];
+            // insert the token into the form so it gets submitted to the server
+            form$.append("<input type='hidden' name='stripeToken' value='" + token + "' />");
+            // and submit
+            form$.get(0).submit();
+        }
+    }
+
+    $(document).ready(function() {
+        $("#payment-form").submit(function(event) {
+            // disable the submit button to prevent repeated clicks
+            $('.submit-button').attr("disabled", "disabled");
+            // createToken returns immediately - the supplied callback submits the form if there are no errors
+            Stripe.createToken({
+                number: $('.card-number').val(),
+                cvc: $('.card-cvc').val(),
+                exp_month: $('.card-expiry-month').val(),
+                exp_year: $('.card-expiry-year').val()
+            }, stripeResponseHandler);
+            return false; // submit from callback
+        });
+    });
+
+    if (window.location.protocol === 'file:') {
+        alert("stripe.js does not work when included in pages served over file:// URLs. Try serving this page over a webserver. Contact support@stripe.com if you need assistance.");
+    }
 </script>
 <!--End of stripe info-->
 <!--Insert Google Analytics code-->
--- dadgumOrder.php
+++ dadgumOrder.php
@@ -6,21 +6,35 @@ phpinfo();
 */
 ?>
 
-<form action="" method="POST" id="payment-form">
+
+<div id="order">
+
+
+</div>
+<form action="index.php" method="POST" id="payment-form">
+    
+	<div>
+	<h3>Welcome to the Dad Gum Order Page</h3>
+	<h5>Please fill in the form below completely and then press Submit Payment</h5>
+	</div>
+	<br />
     <div class="form-row">
         <label>Card Number</label>
         <input type="text" size="20" autocomplete="off" class="card-number"/>
     </div>
+    <br />
     <div class="form-row">
         <label>CVC</label>
         <input type="text" size="4" autocomplete="off" class="card-cvc"/>
     </div>
+    <br />
     <div class="form-row">
         <label>Expiration (MM/YYYY)</label>
         <input type="text" size="2" class="card-expiry-month"/>
         <span> / </span>
         <input type="text" size="4" class="card-expiry-year"/>
     </div>
+    <br />
     <button type="submit" class="submit-button">Submit Payment</button>
 </form>
 
