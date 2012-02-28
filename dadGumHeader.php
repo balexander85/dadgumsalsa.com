8bb2ae17ea1294f20c947ed27f033c8fd8547e9f
Brian Alexander <balexander04@gmail.com>
2012-02-28 16:51:01 -0600

    Nada

--- dadGumHeader.php
+++ dadGumHeader.php
@@ -54,71 +54,71 @@ if (preg_match($blog_pattern, $urlValue)) {
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<link type="image/png" rel="shortcut icon" href="resources/tomato_Favicon.png"/>
-<meta name="author" content="Brian Alexander" />
-<meta name="description" content="Dad Gum Salsa Coming Soon!" />
-<meta name="keywords" content="Dad Gum, Salsa, Dad Gum Salsa, Chips and Salsa" />  
-<title><?php print $title; ?></title>
-<!--Cascading Style Sheets for this Dad'Gum Website--> 
-<link rel="stylesheet" type="text/css" href="dadgumStyle.css" />
-<link rel="stylesheet" type="text/css" href="dadgumStyleBlog.css" />
-<!--This is for the photo gallery-->
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<link rel="stylesheet" type="text/css" href="dadgumStyleGallery.css" />
-<link rel="stylesheet" type="text/css" href="resources/fancy.css" />
-<script type="text/javascript" src="js/jquery-1.2.3.pack.js"></script>
-<script type="text/javascript" src="js/jquery.fancybox-1.0.0.js"></script>
-<script type="text/javascript">
-	$(function(){		
-		$(".photo-link").fancybox({ 'zoomSpeedIn': 500, 'zoomSpeedOut': 500, 'overlayShow': true }); 	
-	});
-</script>
-<!--end of head elements for photo gallery-->
-<!--Beginning of stripe info-->
-<script type="text/javascript" src="https://js.stripe.com/v1/"></script>
-<script type="text/javascript">
-    // this identifies your website in the createToken call below
-    Stripe.setPublishableKey('pk_QER18kLZvhxEoM0oNGISon8fM5onr');
+	<link type="image/png" rel="shortcut icon" href="resources/tomato_Favicon.png"/>
+	<meta name="author" content="Brian Alexander" />
+	<meta name="description" content="Dad Gum Salsa Coming Soon!" />
+	<meta name="keywords" content="Dad Gum, Salsa, Dad Gum Salsa, Chips and Salsa" />  
+	<title><?php print $title; ?></title>
+	<!--Cascading Style Sheets for this Dad'Gum Website--> 
+	<link rel="stylesheet" type="text/css" href="dadgumStyle.css" />
+	<link rel="stylesheet" type="text/css" href="dadgumStyleBlog.css" />
+	<!--This is for the photo gallery-->
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link rel="stylesheet" type="text/css" href="dadgumStyleGallery.css" />
+	<link rel="stylesheet" type="text/css" href="resources/fancy.css" />
+	<script type="text/javascript" src="js/jquery-1.2.3.pack.js"></script>
+	<script type="text/javascript" src="js/jquery.fancybox-1.0.0.js"></script>
+	<script type="text/javascript">
+		$(function(){		
+			$(".photo-link").fancybox({ 'zoomSpeedIn': 500, 'zoomSpeedOut': 500, 'overlayShow': true }); 	
+		});
+	</script>
+	<!--end of head elements for photo gallery-->
+	<!--Beginning of stripe info-->
+	<script type="text/javascript" src="https://js.stripe.com/v1/"></script>
+	<script type="text/javascript">
+	    // this identifies your website in the createToken call below
+	    Stripe.setPublishableKey('pk_QER18kLZvhxEoM0oNGISon8fM5onr');
 
-    function stripeResponseHandler(status, response) {
-        if (response.error) {
-            // re-enable the submit button
-            $('.submit-button').removeAttr("disabled");
-            // show the errors on the form
-            $(".payment-errors").html(response.error.message);
-        } else {
-            var form$ = $("#payment-form");
-            // token contains id, last4, and card type
-            var token = response['id'];
-            // insert the token into the form so it gets submitted to the server
-            form$.append("<input type='hidden' name='stripeToken' value='" + token + "' />");
-            // and submit
-            form$.get(0).submit();
-        }
-    }
+	    function stripeResponseHandler(status, response) {
+	        if (response.error) {
+	            // re-enable the submit button
+	            $('.submit-button').removeAttr("disabled");
+	            // show the errors on the form
+	            $(".payment-errors").html(response.error.message);
+	        } else {
+	            var form$ = $("#payment-form");
+	            // token contains id, last4, and card type
+	            var token = response['id'];
+	            // insert the token into the form so it gets submitted to the server
+	            form$.append("<input type='hidden' name='stripeToken' value='" + token + "' />");
+	            // and submit
+	            form$.get(0).submit();
+	        }
+	    }
 
-    $(document).ready(function() {
-        $("#payment-form").submit(function(event) {
-            // disable the submit button to prevent repeated clicks
-            $('.submit-button').attr("disabled", "disabled");
-            // createToken returns immediately - the supplied callback submits the form if there are no errors
-            Stripe.createToken({
-                number: $('.card-number').val(),
-                cvc: $('.card-cvc').val(),
-                exp_month: $('.card-expiry-month').val(),
-                exp_year: $('.card-expiry-year').val()
-            }, stripeResponseHandler);
-            return false; // submit from callback
-        });
-    });
+	    $(document).ready(function() {
+	        $("#payment-form").submit(function(event) {
+	            // disable the submit button to prevent repeated clicks
+	            $('.submit-button').attr("disabled", "disabled");
+	            // createToken returns immediately - the supplied callback submits the form if there are no errors
+	            Stripe.createToken({
+	                number: $('.card-number').val(),
+	                cvc: $('.card-cvc').val(),
+	                exp_month: $('.card-expiry-month').val(),
+	                exp_year: $('.card-expiry-year').val()
+	            }, stripeResponseHandler);
+	            return false; // submit from callback
+	        });
+	    });
 
-    if (window.location.protocol === 'file:') {
-        alert("stripe.js does not work when included in pages served over file:// URLs. Try serving this page over a webserver. Contact support@stripe.com if you need assistance.");
-    }
-</script>
-<!--End of stripe info-->
-<!--Insert Google Analytics code-->
-<!--End of Google Analytics code-->
+	    if (window.location.protocol === 'file:') {
+	        alert("stripe.js does not work when included in pages served over file:// URLs. Try serving this page over a webserver. Contact support@stripe.com if you need assistance.");
+	    }
+	</script>
+	<!--End of stripe info-->
+	<!--Insert Google Analytics code-->
+	<!--End of Google Analytics code-->
 </head>
 <body id="<?php print $bodyid; ?>">
 
