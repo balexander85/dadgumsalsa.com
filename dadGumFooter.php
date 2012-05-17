b1a277176bad9e61660ae43b8beda62310eabb83
Brian Alexander <balexander04@gmail.com>
2012-05-16 22:43:44 -0500

    Added a twitter button just above the footer. May need to find a better place for it.

--- dadGumFooter.php
+++ dadGumFooter.php
@@ -1,7 +1,18 @@
+<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.dadgumsalsa.com" 
+data-text="Dad Gum Salsa" data-via="dadgumsalsa" data-hashtags="handcraftedsalsa">Tweet</a>
+<script>
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];
+	if(!d.getElementById(id)){js=d.createElement(s);
+		js.id=id;js.src="//platform.twitter.com/widgets.js";
+		fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+</script>
+
+
 <div id="footerPage">
 
 <a href="mailto:info@dadgumsalsa.com"><div id="contactUs"></div></a>
 
 </div>
+
 </body> 
 </html>
