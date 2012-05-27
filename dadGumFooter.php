da025cf595b14d5fcfd0208096748e003aa4e6a8
Brian Alexander <balexander04@gmail.com>
2012-05-27 17:34:08 -0500

    Added a twitter button or two.

--- dadGumFooter.php
+++ dadGumFooter.php
@@ -1,11 +1,3 @@
-<!--<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.dadgumsalsa.com" 
-data-text="Dad Gum Salsa" data-via="dadgumsalsa" data-hashtags="handcraftedsalsa">Tweet</a>
-<script>
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];
-	if(!d.getElementById(id)){js=d.createElement(s);
-		js.id=id;js.src="//platform.twitter.com/widgets.js";
-		fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-</script>-->
 
 
 <div id="footerPage">
@@ -13,6 +5,8 @@ data-text="Dad Gum Salsa" data-via="dadgumsalsa" data-hashtags="handcraftedsalsa
 <a href="mailto:info@dadgumsalsa.com"><div id="contactUs"></div></a>
 
 </div>
+
+
 
 </body> 
 </html>
--- dadGumHeader.php
+++ dadGumHeader.php
@@ -117,3 +117,27 @@ if (preg_match($blog_pattern, $urlValue)) {
 <div id="headerPage"><!--added a png and adjusted the size-->
 <a href="index.php"><div id="linkHome"></div></a>
 </div>
+
+<div id="twitter">
+<a href="https://twitter.com/dadgumsalsa" class="twitter-follow-button" data-show-count="false" data-dnt="true">Follow @dadgumsalsa</a>
+<script>
+!function(d,s,id) {
+	var js,fjs=d.getElementsByTagName(s)[0];
+	if(!d.getElementById(id)) {
+		js=d.createElement(s);js.id=id;
+		js.src="//platform.twitter.com/widgets.js";
+		fjs.parentNode.insertBefore(js,fjs);
+	}
+}(document,"script","twitter-wjs");
+</script>
+
+<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.dadgumsalsa.com" 
+data-text="Dad Gum Salsa" data-via="dadgumsalsa" data-hashtags="handcraftedsalsa">Tweet</a>
+<script>
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];
+       if(!d.getElementById(id)){js=d.createElement(s);
+               js.id=id;js.src="//platform.twitter.com/widgets.js";
+               fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+</script>
+
+</div>
