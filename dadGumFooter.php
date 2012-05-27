4e0c626e075872a2ea18b1fa73c3e5a83b7361f3
Brian Alexander <balexander04@gmail.com>
2012-05-27 14:07:29 -0500

    Commented out twitter button, I'll find a place for that later.

--- dadGumFooter.php
+++ dadGumFooter.php
@@ -1,11 +1,11 @@
-<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.dadgumsalsa.com" 
+<!--<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.dadgumsalsa.com" 
 data-text="Dad Gum Salsa" data-via="dadgumsalsa" data-hashtags="handcraftedsalsa">Tweet</a>
 <script>
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];
 	if(!d.getElementById(id)){js=d.createElement(s);
 		js.id=id;js.src="//platform.twitter.com/widgets.js";
 		fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-</script>
+</script>-->
 
 
 <div id="footerPage">
