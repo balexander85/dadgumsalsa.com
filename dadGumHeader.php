dee6a4422221d7c55f480ea1cf138bd945215d9c
Brian Alexander <balexander04@gmail.com>
2012-05-27 20:08:47 -0500

    Added google analytics to head.

--- dadGumHeader.php
+++ dadGumHeader.php
@@ -110,6 +110,17 @@ if (preg_match($blog_pattern, $urlValue)) {
 	</script>
 	<!--End of stripe info-->
 	<!--Insert Google Analytics code-->
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-32111973-1']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
 	<!--End of Google Analytics code-->
 </head>
 <body id="<?php print $bodyid; ?>">
