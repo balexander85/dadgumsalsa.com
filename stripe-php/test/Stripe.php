5080f0648c470c999c03f68610954ec74bf3ce7d
Brian Alexander <balexander04@gmail.com>
2012-05-27 20:34:02 -0500

    Updated the strip-php repo, which resides in dgs repos.

--- stripe-php/test/Stripe.php
+++ stripe-php/test/Stripe.php
@@ -4,18 +4,14 @@ function authorizeFromEnv()
 {
   $apiKey = getenv('STRIPE_API_KEY');
   if (!$apiKey)
-    throw new Stripe_Error('You need to set STRIPE_API_KEY');
+    $apiKey = "tGN0bIwXnHdwOa85VABjPdSn8nWY7G7I";
   Stripe::setApiKey($apiKey);
 }
 
-if (!getenv('STRIPE_API_KEY')) {
-  echo "MISSING KEY: Provide your Stripe API KEY to this test suite by ".
-       "setting the environmental variable STRIPE_API_KEY. For example:\n".
-       "    $ STRIPE_API_KEY=<your_key> php -f Stripe.php\n";
-  exit(1);
+$ok = @include_once(dirname(__FILE__).'/simpletest/autorun.php');
+if (!$ok) {
+  $ok = @include_once(dirname(__FILE__).'/../vendor/vierbergenlars/simpletest/autorun.php');
 }
-
-$ok = @include_once('simpletest/autorun.php');
 if (!$ok) {
   echo "MISSING DEPENDENCY: The Stripe API test cases depend on SimpleTest. ".
        "Download it at <http://www.simpletest.org/>, and either install it ".
@@ -32,6 +28,7 @@ require_once(dirname(__FILE__) . '/Stripe/AuthenticationErrorTest.php');
 require_once(dirname(__FILE__) . '/Stripe/CardErrorTest.php');
 require_once(dirname(__FILE__) . '/Stripe/ChargeTest.php');
 require_once(dirname(__FILE__) . '/Stripe/CustomerTest.php');
+require_once(dirname(__FILE__) . '/Stripe/DiscountTest.php');
 require_once(dirname(__FILE__) . '/Stripe/Error.php');
 require_once(dirname(__FILE__) . '/Stripe/InvalidRequestErrorTest.php');
 require_once(dirname(__FILE__) . '/Stripe/InvoiceTest.php');
