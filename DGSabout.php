4959873f2c4a7046c206affea6c14b2f295018a4
Brian Alexander <balexander04@gmail.com>
2012-08-24 09:42:59 -0500

    Chea

--- DGSabout.php
+++ DGSabout.php
@@ -4,14 +4,14 @@
 	<img id="orderImg" src="images/DGS_04.png" alt="Dad Gum Salsa Title" />
 	<div id="video" >
 		<center><h2>About Dad Gum Salsa</h2></center>
-		<p>This is some text that is placed in the transparent box.
+<!-- 		<p>This is some text that is placed in the transparent box.
 		I've been making salsa for quite some time now, working on 
 		perfecting my recipes and discovering new types of salsa too. 
 		Ever since I returned from Alaska, I've been trying to think 
 		of what I would really love to do. The one thing I know for 
 		certain, is my love for salsa, it's a passion, 
 		I could do it everyday happily! 
-		</p>
+		</p> -->
 		<object style="width: 640px;height: 368px;" codebase="http://www.apple.com/qtactivex/qtplugin.cab">
 			<param name="src" value="multimedia/videos/dadgumStillMovie.m4v" />
 			<param name="controller" value="true" />
