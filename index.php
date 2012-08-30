f5cc9b3cf07267526663098224f677e5de5b3c4a
Brian Alexander <balexander04@gmail.com>
2012-08-30 18:33:30 -0500

    Added some styling, pushing so I can test on different computers

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <div id="mainFrame">
 <a class="" href="http://www.google.com">
-<img id="frontImage" src="images/DGS_04.png" alt="Index Image" />
+<img id="frontImage" src="images/DGS_16.png" alt="Index Image" />
 </a>
 </div>
 
