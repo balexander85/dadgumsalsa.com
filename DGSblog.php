188744eb328d654b0a1740a790573cc68f7797a4
Brian Alexander <balexander04@gmail.com>
2013-02-14 20:14:04 -0600

    Committing all of my last changes, about to basically destroy and rebuild this branch with django.

--- DGSblog.php
+++ DGSblog.php
@@ -1,5 +1,24 @@
 <?php include "DGS_header.php";?>
-<a href="http://www.google.com">
-<img class="right-image" src="images/DGS_16.png" alt="Dad Gum Salsa Pepper" />
-</a>
+<table>
+  <tr>
+    <td>
+       ..  blog post #1
+       <a href="http://www.google.com">
+		<img class="right-image" src="images/DGS_16.png" alt="Dad Gum Salsa Pepper" />
+		</a>
+    </td>
+    <td>
+    	<a href="http://www.google.com">
+		<img src="images/DGS_16.png" alt="Dad Gum Salsa Pepper" />
+		</a>
+       ..  blog post #2
+    </td>
+    <td>
+    	<a href="http://www.google.com">
+		<img src="images/DGS_16.png" alt="Dad Gum Salsa Pepper" />
+		</a>
+       ..  blog post #3
+    </td>
+  </tr>
+</table>
 <?php include "DGS_footer.php";?>
