048a26dbf8357b17b2a483e5ffc8b4e3e65090fb
Brian Alexander <balexander04@gmail.com>
2013-02-17 10:16:54 -0800

    http://stackoverflow.com/questions/10953695/update-django-1-4-and-python-2-7-3-with-virtualenv-on-dreamhost-with-passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,5 +1,18 @@
 import sys, os
-sys.path.append(os.getcwd())
-os.environ['DJANGO_SETTINGS_MODULE'] = "dadgumsalsa.settings"
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+ cwd = os.getcwd()
+ sys.path.append(cwd)
+ sys.path.append(cwd + '/dadgumsalsa')  #You must add your project here or 500
+
+ #Switch to new python
+ #You may try to replace $HOME with your actual path
+ if sys.version < "2.7.3": os.execl("$HOME/dev.dadgumsalsa.com/env/bin/python",
+     "python2.7.3", *sys.argv)
+
+ sys.path.insert(0,'$HOME/dev.dadgumsalsa.com/env/bin')
+ sys.path.insert(0,'$HOME/dev.dadgumsalsa.com/env/lib/python2.7/site-packages/django')
+ sys.path.insert(0,'$HOME/dev.dadgumsalsa.com/env/lib/python2.7/site-packages')
+
+ os.environ['DJANGO_SETTINGS_MODULE'] = "dadgumsalsa.settings"
+ import django.core.handlers.wsgi
+ application = django.core.handlers.wsgi.WSGIHandler()
+
