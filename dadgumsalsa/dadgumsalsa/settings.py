a8b77ebbf0e455ebc483b00486608611c6fa922a
Brian Alexander <balexander04@gmail.com>
2013-02-17 21:44:12 -0600

    Fixing admin.

--- dadgumsalsa/dadgumsalsa/settings.py
+++ dadgumsalsa/dadgumsalsa/settings.py
@@ -11,11 +11,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'dgs_data',                      # Or path to database file if using sqlite3.
+        'USER': 'dgsalsa',                      # Not used with sqlite3.
+        'PASSWORD': 'Nas1Swimwigy',                  # Not used with sqlite3.
+        'HOST': 'mysql.dhafley.com',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
