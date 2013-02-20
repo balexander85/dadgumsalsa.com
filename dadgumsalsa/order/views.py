919c473b2f780ca9699609744d4ac17de55fa156
Brian Alexander <balexander04@gmail.com>
2013-02-20 13:12:23 -0600

    Temporary Order page.

--- dadgumsalsa/order/views.py
+++ dadgumsalsa/order/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render_to_response
 
 
 def index(request):
-    return render_to_response('order/order_form.html',
+    return render_to_response('order/temp_order_form.html',
                              {'months': range(1, 13),
                              'credit_years': range(2013, 2025)})
