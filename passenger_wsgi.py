40c7a7ff0754e7a2be709ab54540c260e34d7e9c
Brian Alexander <balexander04@gmail.com>
2013-02-20 10:47:11 -0800

    OH my goodness, got it working now! It was some path issues, note to self, $HOME only works in the shell.

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -6,13 +6,14 @@ sys.path.append(cwd + '/dadgumsalsa')  # You must add your project here or 500
  #Switch to new python
  #You may try to replace $HOME with your actual path
 if sys.version < "2.7.3":
-    os.execl("$HOME/dev.dadgumsalsa.com/env/bin/python",
+    os.execl("/home/brianalexander/dev.dadgumsalsa.com/env/bin/python",
              "python2.7.3", *sys.argv)
 
-sys.path.insert(0, '$HOME/dev.dadgumsalsa.com/env/bin')
-sys.path.insert(0, '$HOME/dev.dadgumsalsa.com/env/lib/python2.7/site-packages/django')
-sys.path.insert(0, '$HOME/dev.dadgumsalsa.com/env/lib/python2.7/site-packages')
+sys.path.insert(0, '/home/brianalexander/dev.dadgumsalsa.com/env/bin')
+sys.path.insert(0, '/home/brianalexander/dev.dadgumsalsa.com/env/lib/python2.7/site-packages/django')
+sys.path.insert(0, '/home/brianalexander/dev.dadgumsalsa.com/env/lib/python2.7/site-packages')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = "dadgumsalsa.settings"
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
+
