b39bb2b7b0c4d897a37c2377ecd54f23d1b5bf7a
Brian Alexander <balexander04@gmail.com>
2014-06-10 12:22:36 -0700

    Revert "Clearing up some garbage."
    This reverts commit 25aec92654df1343a7d70e48e4d7d1044fc645a8.

--- dadgumsalsa/portfolio/views.py
+++ dadgumsalsa/portfolio/views.py
@@ -3,6 +3,9 @@ from portfolio.models import daily_balances
 from django.shortcuts import render_to_response
 
 
+chea = 'oof!'
+
+
 def index(request):
     return render_to_response('portfolio/index.html', {
         'entries': daily_balances.objects.all().order_by('log_date').reverse()[:7]
