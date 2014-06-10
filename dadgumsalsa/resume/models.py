d99d6aab292a3af1f87701abba12f5bb20838aa8
Brian Alexander <balexander04@gmail.com>
2014-06-10 00:01:42 -0500

    Upgrading site with db for jobs.

--- dadgumsalsa/resume/models.py
+++ dadgumsalsa/resume/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class resume_experience(models.Model):
+	job_id = models.FloatField()
+	company_name = models.TextField()
+	job_location = models.CharField(max_length=100, unique=False)
+	position_title = models.CharField(max_length=100, unique=False)
+	dates_of_employment = models.CharField(max_length=100, unique=False)
+	job_description = models.TextField()
--- dadgumsalsa/resume/views.py
+++ dadgumsalsa/resume/views.py
@@ -1,5 +1,10 @@
+# Create your views here.
+from resume.models import resume_experience
 from django.shortcuts import render_to_response
 
 
 def index(request):
-    return render_to_response('resume/resume.html')
+    return render_to_response('resume/resume.html', {
+        'jobs': resume_experience.objects.all().order_by('job_id').reverse(),
+    })
+
