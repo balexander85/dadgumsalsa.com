7e5b96a8501519abc4190ecd573dc24a14497935
Brian Alexander <balexander04@gmail.com>
2014-06-10 22:32:42 -0500

    Upddating resume view and model finally..hopefully will work on production.

--- public/dadgumsalsa/resume/views.py
+++ public/dadgumsalsa/resume/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render_to_response
+from resume.models import resume_experience
 
 
 def index(request):
-    return render_to_response('resume/resume.html')
+    return render_to_response('resume/resume.html', {
+        'jobs': resume_experience.objects.all().order_by('id')
+        })
